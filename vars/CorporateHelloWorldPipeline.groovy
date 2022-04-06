57494c862a6ad98a302959ccfb74bd81d387dec2
Jay E <jay.eberhard@gmail.com>
2022-04-06 09:20:02 -0600

    Update CorporateHelloWorldPipeline.groovy

--- vars/CorporateHelloWorldPipeline.groovy
+++ vars/CorporateHelloWorldPipeline.groovy
@@ -1,18 +1,15 @@
-options {
-  durabilityHint 'PERFORMANCE_OPTIMIZED'
-}
-
 def call(Map pipelineParams) {
   pipeline {
-  agent any
-  stages {
-    stage('hello') {
-      steps {
-        helloWorld(name: "${pipelineParams.name}", dayOfWeek: "${pipelineParams.dayOfWeek}")
+    agent any
+    stages {
+      stage('hello') {
+        steps {
+          helloWorld(name: "${pipelineParams.name}", dayOfWeek: "${pipelineParams.dayOfWeek}")
+        }
       }
     }
-  }
-  options {
-    durabilityHint 'PERFORMANCE_OPTIMIZED'
+    options {
+      durabilityHint 'PERFORMANCE_OPTIMIZED'
+    }
   }
 }
