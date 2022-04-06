dfee70d901fcbc66390cb28c04483b89e1a549f8
Jay E <jay.eberhard@gmail.com>
2022-04-06 11:36:17 -0600

    Update CorporateHelloWorldPipelineSequential.groovy

--- vars/CorporateHelloWorldPipelineSequential.groovy
+++ vars/CorporateHelloWorldPipelineSequential.groovy
@@ -1,41 +1,41 @@
 def call(Map pipelineParams) {
-pipeline {
-    agent any
-    stages {
-        stage("build and deploy on Windows and Linux") {
-            parallel {
-                stage("windows") {
-                    stages {
-                        stage("build") {
-                            steps {
-                                sh "echo WindowsBuild"
-                            }
-                        }
-                        stage("deploy") {
-                            when { branch "main" }
-                            steps {
-                                helloWorld(name: "${pipelineParams.name}", dayOfWeek: "${pipelineParams.dayOfWeek}")
-                            }
-                        }
-                    }
-                }
-                stage("linux") {
-                    stages {
-                        stage("build") {
-                            steps {
-                                sh "echo LinuxBuild"
-                            }
-                        }
-                        stage("deploy") {
-                             when { branch "main" }
-                             steps {
-                                helloWorld(name: "${pipelineParams.name}", dayOfWeek: "${pipelineParams.dayOfWeek}")
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+  pipeline {
+      agent any
+      stages {
+          stage("build and deploy on Windows and Linux") {
+              parallel {
+                  stage("windows") {
+                      stages {
+                          stage("build") {
+                              steps {
+                                  sh "echo WindowsBuild"
+                              }
+                          }
+                          stage("deploy") {
+                              //when { branch "main" }
+                              steps {
+                                  helloWorld(name: "${pipelineParams.name}", dayOfWeek: "${pipelineParams.dayOfWeek}")
+                              }
+                          }
+                      }
+                  }
+                  stage("linux") {
+                      stages {
+                          stage("build") {
+                              steps {
+                                  sh "echo LinuxBuild"
+                              }
+                          }
+                          stage("deploy") {
+                               //when { branch "main" }
+                               steps {
+                                  helloWorld(name: "${pipelineParams.name}", dayOfWeek: "${pipelineParams.dayOfWeek}")
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+  }
 }
